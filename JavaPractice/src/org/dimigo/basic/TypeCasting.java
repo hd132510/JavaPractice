5de0e666fb2eabf443d0e63a43073bbe97bb4eab
정혁 <@JeongHyeock>
2015-03-19 12:48:59 +0900

    ...

--- JavaPractice/src/org/dimigo/basic/TypeCasting.java
+++ JavaPractice/src/org/dimigo/basic/TypeCasting.java
@@ -12,7 +12,7 @@ public class TypeCasting {
 		System.out.println("월 평균 급여 : " + String.format("%,d", salary) + "원");
 		System.out.println("점포 내 직원 수 : " + String.format("%d", clerkNum) + "명");
 		System.out.println("점포 수 : " + String.format("%,d", num) + "개\n");
-		System.out.println("연간 인번기 : " + String.format("%,d", yearlySalary) + "원");  
+		System.out.println("연간 인번기 : " + String.format("%,d", yearlySalary) + "원");
 		
 
 	}
