3a46301e070fba4dc809cfbde2f2d4fb095c817d
정혁 <@JeongHyeock>
2015-03-20 14:36:25 +0900

    세번째 실습과제 제출

---
+++ JavaPractice/src/org/dimigo/basic/Operator.java
@@ -0,0 +1,35 @@
+package org.dimigo.basic;
+
+public class Operator {
+
+	public static void main(String[] args) {
+		int a1 = 9;
+		int a2 = 10;
+		double a3 = 5.8;
+		int b1 = 9;
+		double b2 = 5.4;
+		double aArea = (a1+a2)*a3/2;
+		double bArea = b1*b2;
+		String aName = "사다리꼴";
+		String bName = "평행사변형";
+		String area = "넓이";
+		String bigger = " 더 큽니다";
+		
+		System.out.println("<< 도형 넓이 비교 >> ");
+		System.out.println(aName + " " + area + " : " + aArea);
+		System.out.println(bName + " " + area + " : " + bArea + "\n");
+		
+		if(aArea > bArea) {
+			System.out.println(aName + "이 " + bName + " 보다 " + (aArea - bArea) + bigger);
+		}
+		else if(aArea == bArea) {
+			System.out.println(aName + "과 " + bName + " 넓이가 같습니다");
+		}
+		else {
+			System.out.println(bName + "이 " + aName + " 보다 " + (bArea - aArea) + bigger);
+		}
+		
+
+	}
+
+}
