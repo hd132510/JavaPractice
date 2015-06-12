005d6ee2c8aa40a5bda386f63873f1d820b99efe
정혁 <@JeongHyeock>
2015-06-12 14:46:41 +0900

    실습과제 12

---
+++ JavaPractice/src/org/dimigo/oop/FamilyMember.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *   |_ FamilyMember
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 6. 12.
+ * </pre>
+ *
+ * @author		: 김정혁
+ * @version		: 1.0
+ */
+public class FamilyMember {
+	private static int memberCnt = 0;
+	private String memberName;
+	
+	public FamilyMember(String memberName) {
+		this.memberName = memberName;
+		memberCnt++;
+	}
+	
+	public String getMemberName() {
+		return memberName;
+	}
+	public static void printMember() {
+		System.out.println("가족 총 인원수 : " + memberCnt + "명");
+	}
+}
---
+++ JavaPractice/src/org/dimigo/oop/PiggyBankTest.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *   |_ PiggyBankTest
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 6. 12.
+ * </pre>
+ *
+ * @author		: 김정혁
+ * @version		: 1.0
+ */
+public class PiggyBankTest {
+	public static void main(String[] args) {
+		FamilyMember[] f = {
+				new FamilyMember("아빠"),
+				new FamilyMember("엄마"),
+				new FamilyMember("나"),
+				new FamilyMember("남동생")
+		};
+		
+		PiggyBank.putMoney(f[0], 10000);
+		PiggyBank.putMoney(f[1], 5000);
+		PiggyBank.putMoney(f[2], 2000);
+		PiggyBank.putMoney(f[3], 1000);
+		
+		PiggyBank.printBalance();
+		
+		PiggyBank.putMoney(f[2], 1000);
+		PiggyBank.printBalance();
+	}
+
+}
