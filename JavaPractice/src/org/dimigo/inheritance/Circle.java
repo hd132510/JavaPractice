d56482aae00b8909ea0c8138aeb85e4e7475898f
정혁 <@JeongHyeock>
2015-08-12 15:47:18 +0900

    2학기 실습과제1

---
+++ JavaPractice/src/org/dimigo/inheritance/Circle.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   |_ Circle
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 8. 12.
+ * </pre>
+ *
+ * @author		: 김정혁
+ * @version		: 1.0
+ */
+public class Circle extends Figure {
+	private int radius;
+	
+	public Circle(int radius) {
+		this.radius = radius;
+	}
+	
+	public Circle(int centerX, int centerY, int radius) {
+		super(centerX, centerY);
+		this.radius = radius;
+	}
+	
+	protected double calcArea() {
+		return radius * radius * 3.14;
+	}
+}
---
+++ JavaPractice/src/org/dimigo/inheritance/Triangle.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   |_ Triangle
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 8. 12.
+ * </pre>
+ *
+ * @author		: 김정혁
+ * @version		: 1.0
+ */
+public class Triangle extends Figure {
+	private int width;
+	private int height;
+	
+	public Triangle(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+	
+	public Triangle(int centerX, int centerY, int width, int height) {
+		super(centerX, centerY);
+		this.width = width;
+		this.height = height;
+	}
+	
+	protected double calcArea() {
+		return (double) width * height / 2;
+	}
+	
+}
