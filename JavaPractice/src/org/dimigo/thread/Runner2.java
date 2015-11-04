eb3881c14270eb680554baff81e2195ecf3c0ab8
정혁 <@JeongHyeock>
2015-11-04 15:33:25 +0900

    2학기 실습과제 10

---
+++ JavaPractice/src/org/dimigo/thread/Runner2.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package org.dimigo.thread;
+
+/**
+ * <pre>
+ * org.dimigo.thread
+ *   |_ Runner
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 11. 4.
+ * </pre>
+ *
+ * @author		: 김정혁
+ * @version		: 1.0
+ */
+public class Runner2 implements Runnable {
+
+	private String name;
+	
+	public Runner2() {
+		
+	}
+	
+	public Runner2(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public void run() {
+		int d = 110;
+		System.out.println(name + " 출발");
+		
+		while(d != 0) {
+			d -= 10;
+			System.out.println(name + " " + d + "미터");
+			try {
+				Thread.sleep(1000);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.println(name + " 골인");
+	}
+
+}
